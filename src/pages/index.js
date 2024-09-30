ea9a1f726294b0dafa7f42556e03eb4dfd5d5ca1
liviacatela <livia.silva2@estudante.ifms.edu.br>
2024-09-30 15:43:15 +0000

    dia 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,26 @@
-function Index() {
-  return <h1> Olá mundo, estou vivo</h1>
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Index() {
+  return <>
+    <Navbar />
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+    <Footer />
+  </>
 }
-export default Index;
+
+
+
