2ccbc38d4cbacd7ffb4814c6e9dcb81c45471ebb
liviacatela <livia.silva2@estudante.ifms.edu.br>
2024-10-28 15:29:17 +0000

    28/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,20 +1,40 @@
 import { Container } from "react-bootstrap";
 import Menu from "./component/navbar";
 import Footer from "./component/footer";
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 
 export default function Promessa(){
+    var compara = "";
 
-    const [prometido, setPrometido] = useState("")
-    setTimeout(() => {   
-        setPrometido("é uma dívida")
+    if("0" == 0){
+        compara = "Verdadeiro";
+    }else{
+        compara = "False"
+    }
+
+    const [prometido, setPrometido] = useState("");
+    useEffect(() => {
+     setTimeout(() => {   
+        setPrometido(mudaTexto("é uma dívida"));
         document.title="Promessa é dívida"
-    }, 2000);
+    }, 4000);
+
+    });
+    
     return <>
     <Menu />
     <Container>
-        Promessa {prometido===""?<img src=""/>:prometido}
+        Promessa {prometido===""?<img src="load-33_256.gif"/>:prometido}
     </Container>
     <Footer />
     </>
 }
+
+function mudaTexto(Texto){
+    return new Promise((resolva) => {
+        setTimeout(() =>{
+            resolva(texto);
+       
+    },3000);
+   });  
+} 
